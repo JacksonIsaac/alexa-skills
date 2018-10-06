c1e56e5b1c549ba9e96fdffcb871e2873bbc1cb5
Jackson Isaac <jacksonisaac2008@gmail.com>
2018-10-07 00:49:25 +0530

    Add Mumbai Facts skill code

---
+++ mumbai_facts/fact_file.py
@@ -0,0 +1,15 @@
+facts=[
+'Mumbai was earlier known as Bombay.',
+'Mumbai got its name from a popular goddess temple - Mumba Devi Mandir, which is situated at Bori Bunder in Mumbai.',
+'On 16 April 1853, Mumbai witnessed first train movement in India. \
+With 14 carriages and 400 passengers left Bori Bunder, now Chhatrapati Shivaji Terminal, for Thane in Maharashtra.',
+'Often called as Bollywood, Mumbai is the hub of Hindi film industry in India.',
+'Mumbai sea link, officially known as Rajiv Gandhi Sea Link has a length of around 5.6 km. \
+It is an architectural-wonder as it was made with 90,000 tons of cement and steel wire comparable to the girth of the earth.',
+'Ganesh Chaturthi is the most popular 10 day festival in Mumbai which brings the whole city on the street.',
+'Flamingos usually migrate from Afro-Eurasia parts to Mumbai between October to March.',
+'Located in the Andheri West, Gilbert Hill is a 200 ft hill of black basalt rock built around 66 million years ago. \
+It’s unique because only US has a hill made of similar composition.',
+'Way back in 1863, Alice Tredwell, wife of Solomon Tredwell built the railway line from Mumbai to Pune.',
+'1st post office in Mumbai was opened at the junior magistrate’s home in 1832.'
+]
